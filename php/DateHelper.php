57c20eafeae4761513ce8d1c8064a4bd31e4c718
Mesut GENEZ <mesutgenez@hotmail.com>
2021-03-11 13:16:29 +0300

    Update DateHelper.php

--- php/DateHelper.php
+++ php/DateHelper.php
@@ -1,6 +1,9 @@
 <?php
 
-class DateHelperComponent{
+require_once ROOT . DS . APP_DIR . DS . 'Vendor' . DS . 'XLSWriter'. DS .'xlsxwriter.class.php';
+
+class DateHelperComponent extends Component
+{
 
 	public $months = array(
 		'January'       =>    'Ocak',
@@ -50,21 +53,26 @@ class DateHelperComponent{
     // Belirtilen yıl ve ayın çarşamba günlerini key olarak, haftabaşı ve haftasonu tarihlerinide değer olarak geriye dizi döner.
 	public function getWednesdays($y, $m){
 		$wednesday = [];
-		$results =  new DatePeriod(
-			new DateTime("first wednesday of $y-$m"),
-			DateInterval::createFromDateString('next wednesday'),
-			new DateTime("last day of $y-$m")
-		);
+		$begin = new DateTime("first wednesday of $y-$m");
+		$end = new DateTime("last day of $y-$m");
+		$end->setTime(0,0,1);
+		$interval = DateInterval::createFromDateString('next wednesday');
+		$results = new DatePeriod($begin, $interval, $end);
 		foreach ($results as $value) {
-			$monday = strtotime($value->format("d-m-Y")."-9days");
-			$sunday = strtotime($value->format("d-m-Y")."-3days");
+			$monday = strtotime($value->format("d-m-Y")."-10days");
+			$sunday = strtotime($value->format("d-m-Y")."-4days");
 			$wednesday[$value->format("d-m-Y")]["monday"] = date("d-m-Y",$monday);
 			$wednesday[$value->format("d-m-Y")]["sunday"] = date("d-m-Y",$sunday);
 		}
-		$wednesday['last']['monday'] = date('d-m-Y',strtotime("last week"));
-		$wednesday['last']['sunday'] = date('d-m-Y',strtotime("last week +6 days"));
+		$wednesday['last']['monday'] = date('d-m-Y',strtotime("this week"));
+		$wednesday['last']['sunday'] = date('d-m-Y',strtotime("this week +6 days"));
 		$wednesday['last']['first'] = date("Y-m-d", strtotime("wednesday ".$y."-".$m." -9 days"));
 		$wednesday['last']['last'] = date("Y-m-d", strtotime("last wednesday ".$y."-".($m+1)." +5 days"));
+		if($m==12){
+			$wednesday['last']['last'] = date("Y-m-d", strtotime("last wednesday ".($y+1)."-".(1)." +5 days"));
+		}else{
+			$wednesday['last']['last'] = date("Y-m-d", strtotime("last wednesday ".$y."-".($m+1)." +5 days"));
+		}
 		return $wednesday;
     }
 	
@@ -76,7 +84,7 @@ class DateHelperComponent{
 			$years[$value] = $this->getWednesdays($y, $i);
 			$i++;
 		}
-		$years['last']['first'] = date("Y-m-d", strtotime("wednesday ".$y."-01 -2 days"));
+		$years['last']['first'] = date("Y-m-d", strtotime("last wednesday ".($y)."-01 -3 days"));
 		$years['last']['last'] = date("Y-m-d", strtotime("last wednesday ".($y+1)."-01 +5 days"));
 		return $years;
 	}
@@ -108,6 +116,63 @@ class DateHelperComponent{
             $key = $this->weeks[$key];
         }
         return $key; 
+	}
+
+	public function export($weeks){
+		$columns=array(
+			'SALE ID',
+			'PAYMENT ID',
+			'TARIH',
+			'TUTAR'
+		);
+		$payments = [];
+		$refunds = [];
+		for ($i=0; $i<count($weeks['payments']['saleid']); $i++) { 
+			array_push($payments,[
+				$weeks['payments']['saleid'][$i],
+				$weeks['payments']['paymentid'][$i],
+				$weeks['payments']['date'][$i],
+				str_replace('.',',',$weeks['payments']['price'][$i])
+			]);
+		}
+		for ($i=0; $i<count($weeks['refunds']['saleid']); $i++) { 
+			array_push($refunds,[
+				$weeks['refunds']['saleid'][$i],
+				$weeks['refunds']['paymentid'][$i],
+				$weeks['refunds']['date'][$i],
+				str_replace('.',',',$weeks['refunds']['price'][$i])
+			]);
+		}
+		$exportFileName = $weeks['title'].'.xlsx';
+		header('Content-Encoding: UTF-8');
+		header('Content-disposition: attachment; filename="'.XLSXWriter::sanitize_filename($exportFileName).'"');
+		header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		header('Content-Transfer-Encoding: binary');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		$writer = new XLSXWriter();
+		$writer->writeSheetRow('Odemeler', $columns);
+		$writer->writeSheetRow('Iadeler', $columns);
+		foreach($payments as $row) {
+			$writer->writeSheetRow('Odemeler', $row);
+		}
+		foreach($refunds as $row) {
+			$writer->writeSheetRow('Iadeler', $row);
+		}
+		$writer->writeToStdOut();
+		exit(0);
+	}
+
+	public function nextMonth(){
+		$year = date('Y');
+		$month = date('m');
+		if($month==12){
+			$year = date('Y', strtotime('+1 year'));
+			$month = date('01');
+		}else{
+			$month = date('m', strtotime('+1 month'));
+		}
+		return date("$year-$month-01 00:00:00");
 	}
 	
 	
